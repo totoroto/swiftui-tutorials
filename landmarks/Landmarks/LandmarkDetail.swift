3d50b14d85e287b402d1fbec84f8a6c3ea18e72e
totoroto <lsb0496@gmail.com>
2020-07-26 23:00:44 +0900

    Section3 까지 중간 저장

--- landmarks/Landmarks/LandmarkDetail.swift
+++ landmarks/Landmarks/LandmarkDetail.swift
@@ -8,7 +8,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LandmarkDetail: View {
     var body: some View {
         VStack {
             MapView()
@@ -36,8 +36,8 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LandmarkDetail()
     }
 }
---
+++ landmarks/Landmarks/Models/Landmark.swift
@@ -0,0 +1,41 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by pandora on 2020/07/26.
+//  Copyright © 2020 pandora. All rights reserved.
+//
+
+import CoreLocation
+import SwiftUI
+
+struct Landmark: Codable, Hashable {
+    var name: String
+    var category: Category
+    var city: String
+    var state: String
+    var id: Int
+    var park: String
+    var coordinates: Coordinates
+    var imageName: String
+    var locationCoordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: coordinates.latitude, longitude: coordinates.longitude)
+    }
+    
+    struct Coordinates: Codable, Hashable {
+        var latitude: Double
+        var longitude: Double
+    }
+    
+    enum Category: String, Codable, Hashable, CaseIterable {
+        case featured = "Featured"
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+    }
+}
+
+extension Landmark {
+    var image: Image {
+        ImageStore.shared.image(name: imageName)
+    }
+}
